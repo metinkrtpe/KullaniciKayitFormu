ce7c8a3a1e471c87dd97c697b737b582b524a217
Metin <karatepemetin539@hotmail.com>
2021-01-25 17:34:06 +0300

    no message

--- KullaniciKayitFormu/ViewController.swift
+++ KullaniciKayitFormu/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var userText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    
+    @IBOutlet weak var password2Text: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
+    @IBAction func signupClicked(_ sender: Any) {
+        if userText.text == ""{
+            let alert = UIAlertController(title: "Hata", message: "Kullanici bulunamadi", preferredStyle: UIAlertController.Style.alert)
+            let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+            alert.addAction(okButton)
+            self.present(alert, animated: true, completion: nil)
+        }else if passwordText.text == ""{
+            let alert = UIAlertController(title: "Hata", message: "Sifre bulunamadi", preferredStyle: UIAlertController.Style.alert)
+            let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+            alert.addAction(okButton)
+            self.present(alert, animated: true, completion: nil)
+        }else if passwordText.text != password2Text.text{
+            let alert = UIAlertController(title: "Hata", message: "Sifreler eslesmiyor", preferredStyle: UIAlertController.Style.alert)
+            let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+            alert.addAction(okButton)
+            self.present(alert, animated: true, completion: nil)
+        }else{
+            let alert = UIAlertController(title: "Basarili", message: "Basari ile yapildi", preferredStyle: UIAlertController.Style.alert)
+            let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+            alert.addAction(okButton)
+            self.present(alert, animated: true, completion: nil)
+        }
+        
+    }
+    
 
 }
 
